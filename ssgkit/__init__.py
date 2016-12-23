92728df4136ff0bbd71852bb2638863465a14a35
Philip Horger <campadrenalin@gmail.com>
2016-12-23 08:23:47 -0800

    Lazy properties

--- ssgkit/__init__.py
+++ ssgkit/__init__.py
@@ -16,11 +16,28 @@ def write(path, data, mode='w'):
     with open(path, mode) as f:
         f.write(data)
 
+
+def lazy(fn):
+    '''
+    Decorator that makes a property lazy-evaluated.
+    See http://stevenloria.com/lazy-evaluated-properties-in-python/
+    '''
+    attr_name = '_lazy_' + fn.__name__
+
+    def _getter(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    def _setter(self, value):
+        setattr(self, attr_name, value)
+
+    return property(fget=_getter, fset=_setter)
+
 class Page(object):
     def __init__(self, ssg, source_path):
         self.ssg = ssg
         self.source_path = source_path
-        self._split = None
 
     def __repr__(self):
         return "(Page %s -> %s)" % (self.source_path, self.output_path())
@@ -35,37 +52,47 @@ class Page(object):
     def get_content(self):
         return slurp(self.source_path)
 
-    def split_content(self):
-        return self.get_content().split("\n---\n", 1)
+    @lazy
+    def content(self):
+        return slurp(self.source_path)
 
-    def get_frontmatter(self):
-        self._split = self._split or self.split_content()
-        if len(self._split) < 2:
-            return {}
-        if len(self._split) == 2:
-            return json.loads(self._split[0])
+    @lazy
+    def split(self):
+        return self.content.split("\n---\n", 1)
 
-    @property
+    @lazy
     def frontmatter(self):
-        return self.get_frontmatter()
+        split = self.split
+        if len(split) < 2:
+            return {}
+        if len(split) == 2:
+            return json.loads(split[0])
 
-    @property
+    @lazy
     def layout(self):
         return self.frontmatter.get('layout', 'page.html')
 
+    @lazy
+    def title(self):
+        return self.frontmatter.get('title', os.path.basename(self.source_path))
+
     @property
     def template_data(self):
+        '''
+        Deliberately not overrideable directly. Can be modified by modifying
+        frontmatter or top-level attributes like self.title.
+        '''
         f = self.frontmatter
         return dict(f,
-            content = markdown.markdown(self.get_md()), 
-            title   = f.get('title', os.path.basename(self.source_path)),
+            content = markdown.markdown(self.md),
+            title   = self.title,
         )
 
-    def get_md(self):
-        self._split = self._split or self.split_content()
-        return self._split[-1]
+    @lazy
+    def md(self):
+        return self.split[-1]
 
-    def get_html(self):
+    def __str__(self):
         try:
             layout = self.ssg.layouts[self.layout]
         except KeyError:
@@ -104,7 +131,7 @@ class SSG(object):
 
     def build(self):
         for p in self.pages:
-            write(p.output_path(), p.get_html())
+            write(p.output_path(), str(p))
 
         for s in self.static:
             write(
